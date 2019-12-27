e066c4165184056d17d43d5584cb5cac8542e9c1
aramis <aramismario@gmail.com>
2019-12-27 01:33:53 -0500

    interface for vehicles model was added

--- src/models/vehicles.ts
+++ src/models/vehicles.ts
@@ -1,8 +1,14 @@
-import {Schema, model} from "mongoose";
+import {Schema, model, Document} from "mongoose";
+
+
+interface IVehicle extends Document{
+    licenseplate:String,
+    refvehicle:Schema.Types.ObjectId
+}
 
 const vehicleSchema = new Schema({
     licenseplate:{type:String,required:true, unique:true},
     refvehicle:{type:Schema.Types.ObjectId, ref:'refvehicle'}
 });
 
-export default model('vehicles',vehicleSchema);
+export default model<IVehicle>('vehicles',vehicleSchema);
