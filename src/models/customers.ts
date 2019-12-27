ad1b995433363514045032aa1f7e79d26d686807
aramis <aramismario@gmail.com>
2019-12-27 01:30:53 -0500

    adding interfaces to the customer model

--- src/models/customers.ts
+++ src/models/customers.ts
@@ -1,4 +1,18 @@
-import {Schema, model} from "mongoose";
+import {Schema, model, Document} from "mongoose";
+import Bcrypt from "bcrypt";
+
+interface ICustomer extends Document{
+    identificationnumber:String,
+    firstname:String,
+    lastname:String,
+    email:String,
+    password:String,
+    vehicles:[
+        Schema.Types.ObjectId
+    ],
+    encryptPassword(password:String):Promise<String>
+    validatePassword(password:String):Promise<Boolean>
+}
 
 const customerSchema = new Schema({
     identificationnumber:{type:String, required:true, unique:true},
@@ -8,8 +22,19 @@ const customerSchema = new Schema({
     password:{type:String, required:true},
     vehicles:[{
         type:Schema.Types.ObjectId,
-        ref:'vehicles'
-    }]
+        ref:'vehicles',
+        required:false
+    }],
+
 });
 
-export default model('customers',customerSchema);
+customerSchema.methods.encryptPassword = async (password:String):Promise<String> =>  {
+    const salt = await Bcrypt.genSalt(10);
+    return Bcrypt.hash(password,salt);
+}
+
+customerSchema.methods.validatePassword = async function (password:String):Promise<Boolean>{
+    return Bcrypt.compare(password,this.password);
+}
+
+export default model<ICustomer>('Customers',customerSchema);
