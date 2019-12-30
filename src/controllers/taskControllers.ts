7f39c7e67b79f96185643544d3af8dcb0291948d
aramis <aramismario@gmail.com>
2019-12-30 01:34:36 -0500

    adding the forgotten estiamtedTime parameter in saveTask

--- src/controllers/taskControllers.ts
+++ src/controllers/taskControllers.ts
@@ -3,8 +3,8 @@ import Tasks from "../models/tasks";
 class TaskController{
 
     async saveTask(req:Request, res:Response){
-        const {task,tag, refAllowed, price} = req.body;
-        const newTask = new Tasks({task,tag,refAllowed,price});
+        const {task,tag, refAllowed, price, estimatedTime} = req.body;
+        const newTask = new Tasks({task,tag,refAllowed,price,estimatedTime});
         const created = await newTask.save();
         res.json(created);
     }
